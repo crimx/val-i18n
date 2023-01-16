f2ace9346d9640f4120f120a52d7f38519ca0af5
crimx <straybugs@gmail.com>
2023-01-16 14:14:03 +0800

    test: clean code

--- test/i18n.test.ts
+++ test/i18n.test.ts
@@ -1,17 +1,20 @@
 import { describe, it, expect } from "@jest/globals";
+import type { Locales } from "../src/main";
 import { I18n } from "../src/main";
 
 describe("i18n", () => {
   it("smoke test", () => {
-    const i18n = new I18n("en", { en: { name: "CRIMX" } });
+    const locales: Locales = { en: { name: "CRIMX" } };
+    const i18n = new I18n("en", locales);
     expect(i18n.t("name")).toBe("CRIMX");
   });
 
   it("should update t function when lang changes", () => {
-    const i18n = new I18n("en", {
+    const locales: Locales = {
       en: { apple: "apple" },
       zh: { apple: "苹果" },
-    });
+    };
+    const i18n = new I18n("en", locales);
 
     const enT = i18n.t;
     expect(i18n.t("apple")).toBe("apple");
@@ -22,38 +25,42 @@ describe("i18n", () => {
   });
 
   it("should parse message template", () => {
-    const i18n = new I18n("en", {
+    const locales: Locales = {
       en: { intro: "{{name}} eats {{fruit}}" },
-    });
+    };
+    const i18n = new I18n("en", locales);
     expect(i18n.t("intro", { name: "CRIMX", fruit: "apple" })).toBe(
       "CRIMX eats apple"
     );
   });
 
   it("should support array as args", () => {
-    const i18n = new I18n("en", {
+    const locales: Locales = {
       en: { intro: "{{0}} eats {{1}}" },
-    });
+    };
+    const i18n = new I18n("en", locales);
     expect(i18n.t("intro", ["CRIMX", "apple"])).toBe("CRIMX eats apple");
   });
 
   it("should pick option message", () => {
-    const option = (n: number) => (n <= 5 ? "few" : "many");
-
-    const i18n = new I18n("en", {
+    const locales: Locales = {
       en: {
         apple: {
           few: "Few apples",
           many: "Many apples",
         },
       },
-    });
+    };
+
+    const option = (n: number) => (n <= 5 ? "few" : "many");
+
+    const i18n = new I18n("en", locales);
     expect(i18n.t("apple", { ":option": option(1) })).toBe("Few apples");
     expect(i18n.t("apple", { ":option": option(6) })).toBe("Many apples");
   });
 
   it("should pick plural message", () => {
-    const i18n = new I18n("en", {
+    const locales: Locales = {
       en: {
         apple: {
           0: "No apple",
@@ -61,7 +68,8 @@ describe("i18n", () => {
           other: "{{:option}} apples",
         },
       },
-    });
+    };
+    const i18n = new I18n("en", locales);
     expect(i18n.t("apple", { ":option": 0 })).toBe("No apple");
     expect(i18n.t("apple", { ":option": 1 })).toBe("An apple");
     expect(i18n.t("apple", { ":option": 3 })).toBe("3 apples");
