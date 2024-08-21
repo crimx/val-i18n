f453614a8d22c99aa99f1f677172e3f58887af8b
CRIMX <straybugs@gmail.com>
2024-08-21 17:32:42 +0800

    chore: update dev deps

--- jest.config.js
+++
@@ -1,9 +0,0 @@
-/* eslint-env node */
-/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
-module.exports = {
-  preset: "ts-jest",
-  testEnvironment: "node",
-  collectCoverage: true,
-  collectCoverageFrom: ["src/**/*.ts"],
-  coverageReporters: ["clover", "json", "lcov", "text"],
-};
